9054903182656eb74c90e4422cf0211274d96432
Nik Floden <nfloden@gmail.com>
2018-04-09 23:23:49 -0600

    Prim's has been banged out however how to make it show the edges still needs to be figured out. The input is 100% spotless now.

--- src/project3/Matrix.java
+++ src/project3/Matrix.java
@@ -4,6 +4,9 @@
  * and open the template in the editor.
  */
 package project3;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
 
 /**
  *
@@ -11,18 +14,65 @@ package project3;
  */
 public class Matrix {
     int[][] ma;
+    String[] ids;
+    int vertices = 0;
     
-    public Matrix(int size){
-        ma = new int[size + 1][size];
+    public Matrix(int size, String[] split){
+        ma = new int[size][size];
+        ids = split;
     }
     
     public void insertMat(String[] len, int id){
+        System.out.println(len.length);
         for(int i = 0; i < len.length; i++){
             if(len[i].equals("∞")){
                 ma[id][i] = Integer.MAX_VALUE;
             }else{
                 ma[id][i] = Integer.parseInt(len[i]);
             }
+        }
+    }
+    
+    public boolean isEdge(int src, int tgt){
+        return ma[src][tgt]>0;
+    }
+    public void removeEdge(int src, int tgt) {
+        ma[src][tgt] = 0;
+    }
+    public void PrimJarnik(){
+        int min;
+        int w = 0;
+        List<Integer> visited = new ArrayList<>();
+        visited.add(0);
+        while(visited.size() != ids.length){
+            min = Integer.MAX_VALUE;
+            for(int i : visited){
+                for(int j = 0; j < ids.length;++j){
+                    if(min > ma[i][j] && !visited.contains(j)){
+                        min = ma[i][j];
+                        w = j;
+                    }
+                }
+            }
+            visited.add(w);
+        }
+        for(int i = 0; i < visited.size(); i++){
+            System.out.print(visited.get(i));
+        }
+    }
+    
+    public class element{
+        String start, end;
+        int weight;
+        
+        public element(String start, String end, int weight, int priority){
+            this.start = start;
+            this.end = end;
+            this.weight = weight;
+        }
+        
+        public int getWeight(){
+            return weight;
         }
     }
 }
